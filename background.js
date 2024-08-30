ebdea94657ae4fa2670a569e51276e0e6a34d7f4
Dwayne <dwayne@samelogic.com>
2024-08-30 15:53:36 -0400

    feat(background): add onboarding URL on extension install
    - Add logic to open an onboarding URL when the extension is installed.

--- background.js
+++ background.js
@@ -1,4 +1,10 @@
-chrome.runtime.onInstalled.addListener(function () {
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason === "install") {
+    chrome.tabs.create({
+      url: "https://samelogic.com/csspathselector/onboarding",
+    });
+  }
+
   chrome.runtime.setUninstallURL(
     "https://samelogic.com/csspathselector/uninstall"
   );
